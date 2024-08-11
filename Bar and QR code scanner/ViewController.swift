16efd3d5582fb82eea94f145de39a8d9fa62c343
Atik hasan <atikh9979@gmail.com>
2024-08-11 22:11:40 +0600

    Initial Commit

---
+++ Bar and QR code scanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bar and QR code scanner
+//
+//  Created by Atik Hasan on 8/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
